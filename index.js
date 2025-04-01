9ff41523390e9bff3d3600885061e7bb135ab62b
shaninishad1999 <shaninishad1999@gmail.com>
2025-04-01 20:23:44 +0530

    img

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const jsonServer = require("json-server"); // importing json-server library
+// const jsonServer = require("json-server"); // importing json-server library
+// const server = jsonServer.create();
+// const router = jsonServer.router("db.json");
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 3001; //  chose port from here like 8080, 3001
+
+// server.use(middlewares);
+// server.use(router);
+
+// server.listen(port);
+
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 3001;
+
+// Serve static files from the "productImg" folder
+server.use("/productImg", jsonServer.defaults({ static: "productImg" }));
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
